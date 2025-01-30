a9d41506dc12085fdd79a3a3cfa69a1ec51ab15d
Gbayesola Emmanuel <emmanuelgbayes3@gmail.com>
2025-01-30 10:32:27 +0100

    Fix

--- src/main/java/com/ecobank/socket/SocketIOConfig.java
+++ src/main/java/com/ecobank/socket/SocketIOConfig.java
@@ -22,7 +22,7 @@ public class SocketIOConfig {
         // Configure the Socket.IO server
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
         config.setHostname("localhost");
-        config.setPort(9092);
+        config.setPort(8080);
 
         // Create the SocketIO server
         SocketIOServer server = new SocketIOServer(config);
